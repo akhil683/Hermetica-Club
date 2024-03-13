9ce53ff4fa1e21bdf27af5488f031cae45564d97
akhil683 <onkar.palsra@gmail.com>
2024-03-13 23:57:06 +0530

    component for carousel & changed project in about section

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
         </h1>
         <p className='text-justify text-subMainText max-w-[800px] m-auto'>
         Team Hermetica, was established as the departmental team of Chemical Engineering, at the National Institute of Technology, Hamirpur in the year 2014. <br /> <br />
-        "We react to what matters" to nurture adroit intellect in the field of Chemical Engineering. We play an important role in emergent and planned development by conducting advanced research in splendid way. <br /> <br />
-        We provide as a venue for continuing technological and non-technical ideas. TeamHermetica collaborates on initiatives such as bladeless wind turbine, squidbin, natural pesticide, passive cooler and many more projects to with the use of modish technologies. <br /> <br />
+        "We React To What Matters" to nurture adroit intellect in the field of Chemical Engineering. We play an important role in emergent and planned development by conducting advanced research in splendid way. <br /> <br />
+        We provide as a venue for continuing technological and non-technical ideas. Team Hermetica collaborates on initiatives such as BioLume AquaScape, Noise Proofing by Upcycling Agro-waste , Ink from Carbon Soot, Industrial Pipe Cleaning Automated Machine and many more projects to with the use of modish technologies. <br /> <br />
         Along with projects, we had enthralling and wonderful activities like chem-e-car, vaccum gun, aqua rocket which gives plateform to young ones to showcase their innovative ideas, and conducting seminars and exhibitions that give students with a variety of possibilities to enlighten their intriguing minds.
         </p>
       </FramerReveal>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Achievements from '../components/Home/Achievements';
 import Domains from '../components/Home/Domains';
 import FramerReveal from '../components/FramerReveal';
 import Hero from '../components/Home/Hero';
@@ -8,91 +9,13 @@ import AdityaSir from '../components/Home/AdityaSir';
 import NikhilSir from '../components/Home/NikhilSir';
 import Work from '../components/Home/Work';
 
-const Home = () => {
 
+const Home = () => {
   return (
     <>
       <Hero /> 
       <Nimbus />
-      <FramerReveal>
-        <div className='relative flex mb-16 flex-col justify-center items-center gap-12 overflow-hidden'>
-          <h3 className='text-3xl sm:text-5xl'>Past <span className='text-violet'>Achievement</span></h3>
-            <div className="carousel carousel-center w-full px-6 md:max-w-[900px] p-4 space-x-4 rounded-box">
-
-            <div className="carousel-item relative">
-              <div className='absolute top-0 left-0 w-full opacity-50 h-full rounded-2xl overflow-hidden'>
-                <img className='w-full h-full object-cover' src="https://firebasestorage.googleapis.com/v0/b/hermetica-76788.appspot.com/o/Gallery%2Fnimbux%2FIMG_7675.jpg?alt=media&token=ec511c06-bf5f-4ddd-8971-f5d0841afcd9" alt="" />
-              </div>
-              <div className='w-[300px] z-20 h-[400px] p-8 rounded-2xl bg-iconBg bg-opacity-50 flex flex-col justify-center items-center'>
-                <h4 className='mb-8 text-2xl text-violet'>Nimbus 2k16</h4>
-                <p className='text-center text-xl'>Best Publicity Team in Nimbus</p>
-              </div>
-            </div> 
-
-            <div className="carousel-item relative">
-              <div className='absolute top-0 left-0 w-full opacity-50 h-full rounded-2xl overflow-hidden'>
-                <img className='w-full h-full object-cover' src="https://firebasestorage.googleapis.com/v0/b/hermetica-76788.appspot.com/o/Gallery%2Fnimbux%2FIMG_7657.jpg?alt=media&token=a27fda5e-4b1c-4990-a862-c69ad2251962" alt="" />
-              </div>
-              <div className='w-[300px] h-[400px] p-8 rounded-2xl bg-iconBg z-20 bg-opacity-50 flex flex-col justify-center items-center'>
-                <h4 className='mb-8 text-2xl text-violet'>Nimbus 2k17</h4>
-                <p className='text-center text-xl'>Best Event in Nimbus 2k17</p>
-              </div>
-            </div> 
-
-            <div className="carousel-item relative">
-              <div className='absolute top-0 left-0 w-full opacity-50 h-full rounded-2xl overflow-hidden'>
-                <img className='w-full h-full object-cover' src="https://firebasestorage.googleapis.com/v0/b/hermetica-76788.appspot.com/o/Gallery%2Fnimbux%2FIMG_7679.jpg?alt=media&token=f1fe96b4-cc9e-48d8-8971-52372030d98e" alt="" />
-              </div>
-              <div className='w-[300px] h-[400px] p-8 rounded-2xl bg-iconBg z-20 bg-opacity-50 flex flex-col justify-center items-center'>
-                <h4 className='mb-8 text-2xl text-violet'>Nimbus 2k18</h4>
-                <p className='text-center text-xl'>Best Innovative Team in Nimbus</p>
-              </div>
-            </div> 
-
-            <div className="carousel-item relative">
-              <div className='absolute top-0 left-0 w-full opacity-50 h-full rounded-2xl overflow-hidden'>
-                <img className='w-full h-full object-cover' src="https://firebasestorage.googleapis.com/v0/b/hermetica-76788.appspot.com/o/Gallery%2Fnimbux%2FIMG_7657.jpg?alt=media&token=a27fda5e-4b1c-4990-a862-c69ad2251962" alt="" />
-              </div>
-              <div className='w-[300px] h-[400px] p-8 rounded-2xl bg-iconBg z-20 bg-opacity-50 flex flex-col justify-center items-center'>
-                <h4 className='mb-8 text-2xl text-violet'>Nimbus 2k19</h4>
-                <p className='text-center text-xl'>Best Innovative Team in Nimbus</p>
-              </div>
-            </div> 
-
-            <div className="carousel-item relative">
-              <div className='absolute top-0 left-0 w-full opacity-50 h-full rounded-2xl overflow-hidden'>
-                <img className='w-full h-full object-cover' src="https://firebasestorage.googleapis.com/v0/b/hermetica-76788.appspot.com/o/Gallery%2Fnimbux%2FIMG_7679.jpg?alt=media&token=f1fe96b4-cc9e-48d8-8971-52372030d98e" alt="" />
-              </div>
-              <div className='w-[300px] h-[400px] p-8 rounded-2xl bg-iconBg z-20 bg-opacity-50 flex flex-col justify-center items-center'>
-                <h4 className='mb-8 text-2xl text-violet'>Nimbus 2k21</h4>
-                <p className='text-center text-xl'>Best Management Team in Nimbus</p>
-              </div>
-            </div> 
-
-            <div className="carousel-item relative">
-              <div className='absolute top-0 left-0 w-full opacity-50 h-full rounded-2xl overflow-hidden'>
-                <img className='w-full h-full object-cover' src="https://firebasestorage.googleapis.com/v0/b/hermetica-76788.appspot.com/o/Gallery%2Fnimbux%2FIMG_7675.jpg?alt=media&token=ec511c06-bf5f-4ddd-8971-f5d0841afcd9" alt="" />
-              </div>
-              <div className='w-[300px] h-[400px] p-8 rounded-2xl bg-iconBg z-20 bg-opacity-50 flex flex-col justify-center items-center'>
-                <h4 className='mb-8 text-2xl text-violet'>Nimbus 2k23</h4>
-                <p className='text-center text-xl'>Best Events in Nimbus 2k23</p>
-              </div>
-            </div> 
-
-            <div className="carousel-item relative">
-              <div className='absolute top-0 left-0 w-full opacity-50 h-full rounded-2xl overflow-hidden'>
-                <img className='w-full h-full object-cover' src="https://firebasestorage.googleapis.com/v0/b/hermetica-76788.appspot.com/o/Gallery%2Fnimbux%2FIMG_7679.jpg?alt=media&token=f1fe96b4-cc9e-48d8-8971-52372030d98e" alt="" />
-              </div>
-              <div className='w-[300px] h-[400px] p-8 rounded-2xl bg-iconBg z-20 bg-opacity-50 flex flex-col justify-center items-center'>
-                <h4 className='mb-8 text-2xl text-violet'>Nimbus 2k24</h4>
-                <p className='text-center text-xl'>All set to achieve new milestones !</p>
-              </div>
-            </div> 
-
-          </div>
-        </div>
-      </FramerReveal>
-
+      <Achievements />
       <Work />
       <Domains />
 
